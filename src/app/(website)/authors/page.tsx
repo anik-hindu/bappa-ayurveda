30ffe31252e0ba1b47ebcc1cdcebf1edd35bdf02
Anik Saha <aniksahaearnbd@gmail.com>
2026-08-23 14:11:22 +0600

    fix: add fresh content

--- src/app/(website)/authors/page.tsx
+++ src/app/(website)/authors/page.tsx
@@ -7,6 +7,8 @@ export const metadata: Metadata = {
   title: "Authors",
 };
 
+export const dynamic = "force-dynamic";
+
 export default async function AuthorsPage() {
   const authors = await getAllAuthors();
 
