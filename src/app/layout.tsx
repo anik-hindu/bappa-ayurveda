720368ec99821fe8ec14725352d8ed05391c03bd
Anik Saha <aniksahaearnbd@gmail.com>
2026-08-26 21:03:56 +0600

    refactor: move global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { Cormorant_Garamond, DM_Sans } from "next/font/google";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: {
