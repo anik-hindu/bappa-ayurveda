03b6354620203753cbdad942f16c0de595d6a615
Anik Saha <aniksahaearnbd@gmail.com>
2026-08-26 17:59:14 +0600

    docs(utils): add JSDoc comments to cn function

--- src/lib/cn.ts
+++ src/lib/cn.ts
@@ -1,6 +1,12 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
+/**
+ * Combines multiple class names and resolves Tailwind CSS class conflicts.
+ *
+ * @param inputs - Array of class values (strings, objects, arrays, booleans, undefined, null)
+ * @returns Filtered and merged class string
+ */
+export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
